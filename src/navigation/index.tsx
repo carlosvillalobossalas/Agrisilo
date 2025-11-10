92e4b8c7da56dc1f938339a7bbdc4ccacaf25b27
Carlos <carlosvillalobos247@gmail.com>
2025-11-10 17:17:21 -0600

    service screen added

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -3,17 +3,16 @@ import { createStaticNavigation, StaticParamList } from '@react-navigation/nativ
 import { useAppSelector } from '../store';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from '../screens/auth/Login';
-// import JobsScreen from '../screens/jobs/JobsScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { FirebaseAuthTypes, getAuth, onAuthStateChanged } from '@react-native-firebase/auth';
 import { useDispatch } from 'react-redux';
 import { loginFailure, loginStart, loginSuccess } from '../store/slices/authSlice';
 import CalendarScreen from '../screens/calendar/CalendarScreen';
-import LogoutButton from '../components/LogoutButton';
 import { IconButton, useTheme } from 'react-native-paper';
 import ToDoScreen from '../screens/todos/ToDoScreen';
 import ClientScreen from '../screens/clients/ClientScreen';
 import ConfigScreen from '../screens/config/ConfigScreen';
+import ServiceScreen from '../screens/services/ServiceScreen';
 
 // import SignUp from '../screens/auth/SignUp';
 
@@ -126,6 +125,16 @@ const RootStack = createNativeStackNavigator({
             options: {
                 headerShown: true,
                 headerTitle: 'Agregar Cliente',
+            },
+        },
+        ServiceScreen: {
+            screen: ServiceScreen,
+            headerStyle: {
+                height: 110
+            },
+            options: {
+                headerShown: true,
+                headerTitle: 'Agregar Servicio',
             },
         },
     }
