666d6d689a344b4ecb28dc780ef7817d4cc5eaa7
Carlos <carlosvillalobos247@gmail.com>
2025-11-10 17:21:19 -0600

    clients screen added

---
+++ src/screens/clients/ClientsScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ClientsScreen = () => {
+    return (
+        <View>
+            <Text>ClientScreen</Text>
+        </View>
+    )
+}
+
+export default ClientsScreen
