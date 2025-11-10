3c6bb15c035074d47d9c73376c32152672e16110
Carlos <carlosvillalobos247@gmail.com>
2025-11-10 17:17:30 -0600

    service screen added

---
+++ src/screens/services/ServiceScreen.tsx
@@ -0,0 +1,30 @@
+import { View } from 'react-native'
+import React, { useState } from 'react'
+import { Button, Text, TextInput } from 'react-native-paper'
+import CustomColorPicker from '../../components/CustomColorPicker'
+
+const ServiceScreen = () => {
+    const [selectedColor, setSelectedColor] = useState('#ff0000')
+    return (
+        <View style={{ flex: 1, paddingTop: 10, paddingBottom: 15, paddingHorizontal: 15, gap: 10 }}>
+
+            <View style={{ gap: 5 }}>
+                <Text style={{ fontWeight: 'bold' }}>Nombre</Text>
+                <TextInput placeholder='Ingrese el nombre del servicio' mode='outlined' right={<TextInput.Icon icon={'account-outline'} />} />
+            </View>
+
+
+            <View style={{ gap: 5 }}>
+                <Text style={{ fontWeight: 'bold' }}>Color</Text>
+                <CustomColorPicker selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
+            </View>
+
+
+            <Button style={{ marginTop: 'auto', paddingVertical: 5 }} mode='contained'>
+                <Text style={{ fontWeight: 'bold', color: 'white' }}>Guardar servicio</Text>
+            </Button>
+        </View>
+    )
+}
+
+export default ServiceScreen
