ff095c133abe2759bec8ec92ed1926ad7def5b1f
Carlos <carlosvillalobos247@gmail.com>
2025-11-10 16:12:48 -0600

    client screen ui

--- src/screens/clients/ClientScreen.tsx
+++ src/screens/clients/ClientScreen.tsx
@@ -1,12 +1,41 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View } from 'react-native'
+import { Button, Text, TextInput } from 'react-native-paper'
 
 const ClientScreen = () => {
-    return (
-        <View>
-            <Text>ClientScreen</Text>
-        </View>
-    )
+  return (
+    <View style={{ flex: 1, paddingVertical: 30, paddingHorizontal: 15, gap: 10 }}>
+
+      <View style={{ gap: 5 }}>
+        <Text style={{ fontWeight: 'bold' }}>Nombre</Text>
+        <TextInput placeholder='Ingrese el nombre del cliente' mode='outlined' right={<TextInput.Icon icon={'account-outline'} />} />
+      </View>
+
+      <View style={{ gap: 5 }}>
+        <Text style={{ fontWeight: 'bold' }}>Correo Electronico</Text>
+        <TextInput placeholder='ejemplo@gmail.com' mode='outlined' right={<TextInput.Icon icon={'email-outline'} />} />
+      </View>
+
+      <View style={{ gap: 5 }}>
+        <Text style={{ fontWeight: 'bold' }}>Area (hectáreas)</Text>
+        <TextInput placeholder='0' mode='outlined' right={<TextInput.Icon icon={'terrain'} />} />
+      </View>
+
+      <View style={{ gap: 5 }}>
+        <Text style={{ fontWeight: 'bold' }}>Número de teléfono</Text>
+        <TextInput placeholder='Ingrese el número de teléfono' mode='outlined' right={<TextInput.Icon icon={'phone-outline'} />} />
+      </View>
+
+      <View style={{ gap: 5 }}>
+        <Text style={{ fontWeight: 'bold' }}>Ubicación del terreno</Text>
+        <TextInput placeholder='Ingrese la ubicación del terreno' mode='outlined' right={<TextInput.Icon icon={'map-marker-outline'} />} />
+      </View>
+
+      <Button style={{ marginTop: 'auto', paddingVertical: 5 }} mode='contained'>
+        <Text style={{ fontWeight: 'bold', color: 'white' }}>Guardar cliente</Text>
+      </Button>
+    </View>
+  )
 }
 
 export default ClientScreen
