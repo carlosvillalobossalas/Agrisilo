b6601eb5d941ad2226d11accc136b0d42a3f50b7
Carlos <carlosvillalobos247@gmail.com>
2025-11-11 11:03:09 -0600

    new custom button

---
+++ src/components/CustomButtonTwoIcons.tsx
@@ -0,0 +1,45 @@
+import { View } from 'react-native'
+import React from 'react'
+import { Button, Icon, Text } from 'react-native-paper'
+
+interface CustomButtonTwoIcons {
+    label: string
+    iconLeft: string
+    iconRight: string
+}
+
+const CustomButtonTwoIcons = ({ label, iconLeft, iconRight }: CustomButtonTwoIcons) => {
+    return (
+        <Button
+            mode="elevated"
+            icon={({ size, color }) => (
+                <Icon source={iconLeft} size={30} color={color} /> // 👈 controla el tamaño aquí
+            )}
+            style={{
+                borderRadius: 10,
+                width: '100%',
+                height: 62,
+            }}
+            contentStyle={{
+                height: 62,
+                alignItems: 'center',
+                justifyContent: 'flex-start'
+            }}
+            onPress={() => { }}
+        >
+            <View style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                height: 62,
+                paddingTop: 5,
+                justifyContent: 'space-between',
+                width: '95%',
+            }}>
+                <Text style={{ fontSize: 16 }}>{label}</Text>
+                <Icon source={iconRight} size={20} />
+            </View>
+        </Button>
+    )
+}
+
+export default CustomButtonTwoIcons
