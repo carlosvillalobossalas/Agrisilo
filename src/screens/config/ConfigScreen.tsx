6fe1d0a6b19e2d0d583835ad35433b9a81ae3502
Carlos <carlosvillalobos247@gmail.com>
2025-11-19 19:07:26 -0600

    navigation screen fix

--- src/screens/config/ConfigScreen.tsx
+++ src/screens/config/ConfigScreen.tsx
@@ -18,7 +18,7 @@ const ConfigScreen = () => {
   }
   return (
     <View style={{ flex: 1, paddingVertical: 30, paddingHorizontal: 20, gap: 10 }}>
-      <CustomButtonTwoIcons label={'Mi Perfil'} iconLeft={"account-circle-outline"} iconRight={"chevron-right"} />
+      <CustomButtonTwoIcons label={'Mi Perfil'} iconLeft={"account-circle-outline"} iconRight={"chevron-right"} onPress={() => { navigation.navigate('ProfileScreen') }}/>
       <View style={{ height: 20 }} />
       <CustomButtonTwoIcons label={'Lista de usuarios'} iconLeft={"account-multiple-outline"} iconRight={"chevron-right"} onPress={() => { navigation.navigate('UsersScreen') }} />
       <CustomButtonTwoIcons label={'Lista de servicios'} iconLeft={"account-wrench-outline"} iconRight={"chevron-right"} onPress={() => { navigation.navigate('ServicesScreen') }} />
